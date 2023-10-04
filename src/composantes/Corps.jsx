e5944e404d7bafa60997dda77f17826f7280fab3
abidasakk <tanhaxelkevin@gmail.com>
2023-10-04 12:01:24 +0200

    challenge1

---
+++ src/composantes/Corps.jsx
@@ -0,0 +1,288 @@
+import React from "react";
+function Corps(){
+   
+  return(
+    <> 
+    <div className="accueil">
+         <div className="boites">
+                <div className="boite_gauche">
+                   <div className="ligne">
+                    <div className="facebook">facebook</div>
+                    <div className="options">...</div>
+                    </div>
+                   <div className="lignes" >
+                   <img src="groupes.png" className="logo" alt="" />
+                   <div className="menu_gauche">Friends</div>     
+                    </div>
+                   <div className="lignes" >
+                   <img src="groupes.png" className="logo" alt="" />
+                    <div className="menu_gauche">Marketplace</div>
+                    </div> 
+                   <div className="lignes">
+                   <img src="groupes.png" className="logo" alt="" />
+                     <div className="menu_gauche">Groups</div>
+                    </div>
+                   <div className="lignes">
+                   <img src="groupes.png" className="logo" alt="" />
+                   <div className="menu_gauche"> Watch </div>
+                    </div>
+                   <div className="lignes">
+                   <div> <img src="groupes.png" className="logo" alt="" /> </div>
+                    <div className="menu_gauche">See more</div>
+                    </div>
+
+                </div>
+
+                <div className="boite_gauche">
+                  <div>Gaming</div>
+                  
+                  <div className="gaming_gauches">
+                    <img src="chien.jpg" className="jeux" alt="" />
+                    <div className="jeu_detail">
+                      <div className="theme">dog racing</div>
+                      <div>205 follower</div>
+                    </div>
+                    <div className="jeu_detail">
+                      <div className="themes">follow</div>
+                      <div>125 watching</div>
+                    </div>
+                </div>
+
+                <div className="gaming_gauches">
+                    <img src="chien.jpg" className="jeux" alt="" />
+                    <div className="jeu_detail">
+                      <div className="theme">dog racing</div>
+                      <div>205 follower</div>
+                    </div>
+                    <div className="jeu_detail">
+                      <div className="themes">follow</div>
+                      <div>125 watching</div>
+                    </div>
+                </div>
+
+
+                <div className="lignes">
+                   <div> <img src="groupes.png" className="logo" alt="" /> </div>
+                    <div className="menu_gauche">See more</div>
+                    </div>
+         </div>  
+   </div>
+
+   <div className="boites">
+           <div className="storie">
+            <div className="cadre_image1">
+           <div className="ajouter">+</div>
+           </div>
+            <img src="chien.jpg" alt=""  className="image_produire"/>
+            <img src="manequin.jpg" alt=""  className="image_produire"/>
+            <img src="tenu.webp" alt=""  className="image_produire"/>
+            <img src="residences.jpg" alt=""  className="image_produire"/>
+            <div> <img src="groupes.png" className="logo" alt="" /> </div>
+           </div>
+
+           <div className="actualité">
+             <div className="actu_cadre">
+                <div className="profiles">
+                  <img src="meuble.jpg" className="photo_profile" alt="" />
+                  <div className="profile_detail">
+                    <div>
+                    <div className="nom_profile">
+                      Jane Smith
+                    </div>
+                    <div className="temps_profile">
+                      15 min.<img src="groupes.png" className="logo" alt="" />
+                    </div>
+                    </div>
+
+                    <div>
+                    <div className="options">...</div>
+                    </div>
+
+                  </div>
+
+                </div>
+                <div className="satus">
+                hey friends i have some thlling news to share with
+                all of you i'm absolutely to embark on a new adventure 
+                that's just around.
+
+                </div>
+                <div className="reactions">
+                  <div className="emojie">
+                  <img src="groupes.png" className="logo" alt="" />
+                  <img src="groupes.png" className="logo" alt="" />
+                     you and 1 other
+                  </div>
+                  <div>
+                    0 commentaire 
+                  </div>
+                </div>
+                <div className="reactions">
+                    <div className="like bouttons"> <img src="groupes.png" className="logo" alt="" /> Like</div>
+                    <div className="bouttons"><img src="groupes.png" className="logo" alt="" /> Comment</div>
+                    <div className="bouttons"><img src="groupes.png" className="logo" alt="" /> Share</div>
+
+                </div>
+
+             </div>
+
+
+
+
+
+             <div className="actu_cadre">
+                <div className="profiles">
+                  <img src="meuble.jpg" className="photo_profile" alt="" />
+                  <div className="profile_detail">
+                    <div>
+                    <div className="nom_profile">
+                      Jane Smith
+                    </div>
+                    <div className="temps_profile">
+                      15 min.<img src="groupes.png" className="logo" alt="" />
+                    </div>
+                    </div>
+
+                    <div>
+                    <div className="options">...</div>
+                    </div>
+
+                  </div>
+
+                </div>
+                <div className="satus">
+                hey friends i have some thlling news to share with
+                all of you i'm absolutely to embark on a new adventure 
+                that's just around.
+
+                </div>
+                <div className="reactions">
+                  <div className="emojie">
+                  <img src="groupes.png" className="logo" alt="" />
+                  <img src="groupes.png" className="logo" alt="" />
+                     you and 1 other
+                  </div>
+                  <div>
+                    0 commentaire 
+                  </div>
+                </div>
+                <div className="reactions">
+                    <div className="like bouttons"> <img src="groupes.png" className="logo" alt="" /> Like</div>
+                    <div className="bouttons"><img src="groupes.png" className="logo" alt="" /> Comment</div>
+                    <div className="bouttons"><img src="groupes.png" className="logo" alt="" /> Share</div>
+
+                </div>
+
+             </div>
+           </div>
+
+
+
+
+  </div>
+
+
+   <div className="boites">
+
+          <div className="boite_droit">
+           <div className="btn_droit">
+                    <img src="groupes.png" className="logo espace" alt="" />
+                    <img src="groupes.png" className="logo espace" alt="" />
+                    <img src="groupes.png" className="logo espace" alt="" />
+                   <img src="meuble.jpg" className="photo_profile" alt="" />
+           </div>
+
+           <div className="page">
+                  <div>your page</div>
+                  <div className="profiles">
+                  <img src="meuble.jpg" className="photo_profile" alt="" />
+                  <div className="profile_detail2">
+                    <div>
+                    <div className="nom_profile">
+                      Figma
+                    </div>
+                    <div className="temps_profile">
+                      @figma
+                    </div>
+                    </div>
+
+                    <div>
+                    <img src="groupes.png" className="logo" alt="" />
+                    </div>
+
+                  </div>
+                  </div>
+          </div>
+
+           <div className="cardre_birth">
+              <div className="birth"> birthday</div>
+              <img src="residences.jpg" className="img_birth" alt="" />
+              <div className="lignes">
+                   <div>
+                    clara et 2 other
+                    have birthday
+                   </div>
+                   <img src="meuble.jpg" className="photo_profile" alt="" />
+                   <img src="meuble.jpg" className="photo_profile" alt="" />
+                   <img src="meuble.jpg" className="photo_profile" alt="" />
+                   <div>
+
+                   </div>
+              </div>
+
+           </div>
+
+           <div className="contact_birth">
+               <div>
+                Contact
+               </div>
+
+               <div>
+               <img src="groupes.png" className="logo espace" alt="" />
+               <img src="groupes.png" className="logo espace" alt="" />
+               <img src="groupes.png" className="logo espace" alt="" />
+               
+           </div>
+
+           </div>
+           <div className="contact_profile">
+           <div className="lignes">
+           <img src="meuble.jpg" className="photo_profile" alt="" />
+           <div>sarha kouassi</div>
+           </div>
+           <div className="lignes">
+           <img src="meuble.jpg" className="photo_profile" alt="" />
+           <div>sarha kouassi</div>
+           </div>
+           <div className="lignes">
+           <img src="meuble.jpg" className="photo_profile" alt="" />
+           <div>sarha kouassi</div>
+           </div>
+           <div className="lignes">
+           <img src="meuble.jpg" className="photo_profile" alt="" />
+           <div>sarha kouassi</div>
+           </div>
+           <div className="lignes">
+           <img src="meuble.jpg" className="photo_profile" alt="" />
+           <div>sarha kouassi</div>
+           </div>
+           </div>
+           
+           
+          
+          
+          </div> 
+   </div>
+
+
+
+
+
+
+
+   </div>
+    </>
+  )
+
+}
+export default Corps;
